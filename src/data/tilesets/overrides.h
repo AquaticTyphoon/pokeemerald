a25f186d34536a19aae753a2a85e578f83b2d2a9
AquaticTyphoon <aquatictyphoon@yahoo.com>
2023-03-18 02:48:39 -0500

    Removal of forgotten tiles

--- src/data/tilesets/overrides.h
+++ src/data/tilesets/overrides.h
@@ -1,7 +1,5 @@
 static const u16 sTilesetPalOverride_General01_05[] = INCBIN_U16("data/tilesets/primary/general/override_palettes/01_05.gbapal");
 
-static const u16 sTilesetPalOverride_Petalburg06_08[] = INCBIN_U16("data/tilesets/secondary/petalburg/override_palettes/06_08.gbapal");
-
 #define OVERRIDES_END { .slot = 0xFF, }
 
 const struct PaletteOverride gTilesetPalOverrides_General[] =
@@ -17,23 +15,6 @@ const struct PaletteOverride gTilesetPalOverrides_General[] =
         .startHour = HOUR_NIGHT,
         .endHour = HOUR_MORNING,
         .palette = sTilesetPalOverride_General01_05,
-    },
-    OVERRIDES_END
-};
-
-const struct PaletteOverride gTilesetPalOverrides_Petalburg[] =
-{
-    {
-        .slot = 6,
-        .startHour = HOUR_NIGHT,
-        .endHour = HOUR_MORNING,
-        .palette = sTilesetPalOverride_Petalburg06_08,
-    },
-    {
-        .slot = 8,
-        .startHour = HOUR_NIGHT,
-        .endHour = HOUR_MORNING,
-        .palette = sTilesetPalOverride_Petalburg06_08,
     },
     OVERRIDES_END
 };
